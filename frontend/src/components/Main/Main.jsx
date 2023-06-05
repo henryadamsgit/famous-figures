3eb38fe8ae59486db93c23367d19d3b4cefb3d7e
Henry Adams <henrybruce.adams@icloud.com>
2023-06-05 18:56:12 +0100

    Added links to cards and create new component for routing.

--- frontend/src/components/Main/Main.jsx
+++ frontend/src/components/Main/Main.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import "./Main.scss";
 import Card from "../Card/Card";
+import { NavLink } from "react-router-dom";
 
 const Main = ({ filteredFigures }) => {
   return (
     <div className="main">
       {filteredFigures.map((figure) => (
-        <Card key={figure.id} data={figure} />
+        <div key={figure.id}>
+          <NavLink
+            to={`/figures/${figure.id}`}
+            className="custom-link"
+            activeClassName="active"
+          >
+            <Card data={figure} />
+          </NavLink>
+        </div>
       ))}
     </div>
   );
 };
 
 export default Main;
-
